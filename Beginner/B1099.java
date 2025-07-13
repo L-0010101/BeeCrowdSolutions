4d2965d4b2be5c545caeb21597cc2b5c8808f2b8
LeonardoMelo <65695994+L-0010101@users.noreply.github.com>
2025-07-13 12:47:12 -0300

    Organizing

---
+++ Beginner/B1099.java
@@ -0,0 +1,36 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class B1099 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int result;
+        for (int i = 0; i < N; i++){
+            result = 0;
+            int X = in.nextInt();
+            int Y = in.nextInt();
+            if (X > Y){
+                for (int j = Y+1; j < X; j++) {
+                    String strJ = String.valueOf(j);
+                    if (strJ.endsWith("1") || strJ.endsWith("3") || strJ.endsWith("5") || strJ.endsWith("7") || strJ.endsWith("9")){
+                        result += j;
+                    }
+                }
+                System.out.println(result);
+            }
+            else if (Y > X){
+                for (int j = X+1; j < Y; j++) {
+                    String strJ = String.valueOf(j);
+                    if (strJ.endsWith("1") || strJ.endsWith("3") || strJ.endsWith("5") || strJ.endsWith("7") || strJ.endsWith("9")){
+                        result += j;
+                    }
+                }
+                System.out.println(result);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
