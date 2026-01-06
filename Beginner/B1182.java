9038a899942a096e1170a467c9c1eac0243c97d3
LeonardoMelo <65695994+L-0010101@users.noreply.github.com>
2026-01-05 23:11:08 -0300

    Create B1182.java

---
+++ Beginner/B1182.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class B1182 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double [][] matrix = new double[12][12];
+        int col = in.nextInt();
+        double result = 0;
+        String T = in.next();
+        for (int i = 0; i < 12; i++){
+            for (int j = 0; j < 12; j++) {
+                matrix[j][i] = in.nextDouble();
+            }
+        }
+        if (T.equalsIgnoreCase("S")){
+            for (int i = 0; i < 12; i++) {
+                result += matrix[i][col];
+            }
+            System.out.printf("%.1f \n", result);
+        }
+        else if (T.equalsIgnoreCase("M")){
+            for (int i = 0; i < 12; i++) {
+                result += matrix[i][col];
+            }
+            result /= 12;
+            System.out.printf("%.1f \n", result);
+        }
+    }
+}
